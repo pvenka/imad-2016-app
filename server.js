862919b3e844f9c47ab536ad2abd4b2b6ef36526
pvenka <pvenka@users.noreply.github.com>
2016-10-05 15:57:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,53 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <!DOCTYPE html>
+    <head>
+        <title>
+            ${title}
+        </title>
+         <meta name ="viewport" content="width=device-width, initial-scale=1"/>
+          <link href="/ui/style.css" rel "stylesheet" />
+         <h2> ${heading}</h2>
+    </head>
+    <body>
+   <a href="/"></a>
+   <p>
+   ${content}
+    </p>
+
+    </body>
+</html>`;
+    return htmlTemplate;
+}
+
+var articles = {
+    articleOne: { 
+        title: "First Article",
+        heading: "First page",
+        content: 
+        `This is a content for the first article.
+        We will just write something.
+        We can modify later.
+        `
+    },
+    articleTwo: {},
+    articleThree: {}
+};
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
+   //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
    //res.send("Article one will be served soon!"); 
 });
 app.get('/article-two', function (req, res) {
