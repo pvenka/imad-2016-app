9983cc5b94f9cbf950e36e5926802b3df34c07b4
pvenka <pvenka@users.noreply.github.com>
2016-10-06 15:04:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,8 +62,11 @@ var articles = {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-
+var counter =0;
+app.get('/counter',function(rq, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
