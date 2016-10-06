36d1eb5c83de94ff010cd87f43e83bf821f7855e
pvenka <pvenka@users.noreply.github.com>
2016-10-06 22:31:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 var names =[];
 app.get('/submit-name/:name', function (req,res) {
-    var name = res.params.name;
+    var name = req.params.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
