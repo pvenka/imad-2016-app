532b36955445452b18be9b282714c534d1e86bff
pvenka <pvenka@users.noreply.github.com>
2016-10-06 22:49:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,18 +35,39 @@ request.onreadystatechange = function() {
     request.send(null);
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+//var nameInput = document.getElementById('name');
+//var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
-    var names = ['Name1','Name2','Name3'];
-    var list = '';
+    var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status == 200) {
+            var names = req.responseText;
+            names = JSON.parse(names);
+            var list = '';
     for (i=0;i<names.length;i++) {
         list += '<li>'+names[i]+'</li>';
     }
     ul = document.getElementById('namelist');
     ul.innerHTML = list;
+        }
+    }
+};
+//button.onclick = function() {
+    
+  //  counter = counter +1;
+//    var span = document.getElementById('count');
+  //  span.innerHTML = counter.toString();
+  var nameInput = document.getElementById('name');
+var name = nameInput.value;
+    request.open('GET', 'http://pvenka.imad.hasura-app.io/submit-name/?name=' + name,true);
+    request.send(null);
+    
+    //var names = ['Name1','Name2','Name3'];
+   
 };
 
 
