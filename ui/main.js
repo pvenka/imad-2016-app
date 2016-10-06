889d09a21f75a08b139e292df0144b5f8021650b
pvenka <pvenka@users.noreply.github.com>
2016-10-06 06:40:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
 var img = document.getElementById("maldi");
 img.onclick = function() {
-    img.style.marginleft = "100px";
+    img.style.margin-left = "100px";
 };
